7e43e87cbf5097157693966757b868ec2a5f3900
21520372 <21520372@gm.uit.edu.vn>
2023-11-01 14:22:04 +0700

    chinh sua responesive

--- components.js
+++ components.js
@@ -3,11 +3,7 @@ const components = [
       name: 'navbar',
       url: 'components/navbar/index.html',
       img: 'components/navbar/img/',
-      linksCSS: [
-        'components/navbar/globals.css',
-        'components/navbar/styleguide.css',
-        'components/navbar/style.css'
-      ]
+      linkCSS: 'components/navbar/style.css'
     }
   ];
 
@@ -27,12 +23,10 @@ components.forEach(component => {
         } else {
             console.log(`Element with ID '${component.name}' not found.`);
         }
-        component.linksCSS.forEach(linkCSS =>{
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = linkCSS;
-            document.head.appendChild(link);
-        });
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = component.linkCSS;
+        document.head.appendChild(link);
     })
     .catch(function (err) {
         console.error('Failed to fetch page: ', err);
