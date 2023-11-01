765ffe4eb09a2a3faa2a976d461da2bd916d9a5d
hieu8123 <21522070@gm.uit.edu.vn>
2023-11-01 16:52:50 +0700

    edit project structure

--- components.js
+++ components.js
@@ -1,35 +1,35 @@
 const components = [
     {
-      name: 'navbar',
-      url: 'components/navbar/index.html',
-      img: 'components/navbar/img/',
-      linkCSS: 'components/navbar/style.css'
+        name: 'navbar',
+        url: 'components/navbar.html',
+        img: 'assets/images/navbar/',
+        linkCSS: 'styles/navbar.css'
     }
-  ];
+];
 
 components.forEach(component => {
     fetch(component.url)
-    .then(function (response) {
-        return response.text();
-    })
-    .then(function (html) {
-        var newElement = document.createElement("div");
-        html = html.replace(/img\//g, component.img);
-        newElement.innerHTML = html;
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            var newElement = document.createElement("div");
+            html = html.replace(/img\//g, component.img);
+            newElement.innerHTML = html;
 
-        var oldElement = document.getElementById(component.name);
-        if (oldElement) {
-            oldElement.parentNode.replaceChild(newElement, oldElement);
-        } else {
-            console.log(`Element with ID '${component.name}' not found.`);
-        }
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = component.linkCSS;
-        document.head.appendChild(link);
-    })
-    .catch(function (err) {
-        console.error('Failed to fetch page: ', err);
-    });
+            var oldElement = document.getElementById(component.name);
+            if (oldElement) {
+                oldElement.parentNode.replaceChild(newElement, oldElement);
+            } else {
+                console.log(`Element with ID '${component.name}' not found.`);
+            }
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = component.linkCSS;
+            document.head.appendChild(link);
+        })
+        .catch(function (err) {
+            console.error('Failed to fetch page: ', err);
+        });
 });
 
