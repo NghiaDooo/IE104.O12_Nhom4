70034a50df512f7351207e85ec3e44b3559bcc08
21520372 <21520372@gm.uit.edu.vn>
2023-11-02 10:22:52 +0700

    chinh sua component.js

--- components.js
+++ components.js
@@ -1,35 +1,40 @@
 const components = [
     {
-        name: 'navbar',
-        url: 'components/navbar.html',
-        img: 'assets/images/navbar/',
-        linkCSS: 'styles/navbar.css'
+      name: 'navbar',
+      url: 'components/navbar/index.html',
+      img: {
+        pattern : 'img/',
+        replacement : 'components/navbar/img/',
+      },
+      linkCSS: 'components/navbar/style.css'
     }
-];
+  ];
 
 components.forEach(component => {
     fetch(component.url)
-        .then(function (response) {
-            return response.text();
-        })
-        .then(function (html) {
-            var newElement = document.createElement("div");
-            html = html.replace(/img\//g, component.img);
-            newElement.innerHTML = html;
+    .then(function (response) {
+        return response.text();
+    })
+    .then(function (html) {
+        const newElement = document.createElement("div");
+        const regex = new RegExp(component.img.pattern, 'g');
 
-            var oldElement = document.getElementById(component.name);
-            if (oldElement) {
-                oldElement.parentNode.replaceChild(newElement, oldElement);
-            } else {
-                console.log(`Element with ID '${component.name}' not found.`);
-            }
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = component.linkCSS;
-            document.head.appendChild(link);
-        })
-        .catch(function (err) {
-            console.error('Failed to fetch page: ', err);
-        });
+        html = html.replace(regex, component.img.replacement);
+        newElement.innerHTML = html;
+
+        const oldElement = document.getElementById(component.name);
+        if (oldElement) {
+            oldElement.parentNode.replaceChild(newElement, oldElement);
+        } else {
+            console.log(`Element with ID '${component.name}' not found.`);
+        }
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = component.linkCSS;
+        document.head.appendChild(link);
+    })
+    .catch(function (err) {
+        console.error('Failed to fetch page: ', err);
+    });
 });
 
